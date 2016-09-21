fefb31c385ab92c3b1913750e2ffd992eb181f83
Talon Hughes <talon.hughes@gmail.com>
2016-09-20 19:01:41 -0600

    finished mini project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,2 @@
 angular.module('friendsList', []);
+//variable angular is calling the method module and we are putting it into an empty array
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,3 +1,5 @@
 angular.module('friendsList').controller('mainCtrl', function($scope){
+//$scope is how we interface with the frontend
+$scope.name = 'Talon';
 $scope.friends = ['Lauren', 'Josh', 'Stewart'];
 });
